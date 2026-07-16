148036515f65b572e20ed1b01c60251e3a97f6b4
rakeshv02 <bclodge1@gmail.com>
2026-07-16 08:52:42 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          600: '#2563eb',
+        }
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      }
+    }
+  }
+}
